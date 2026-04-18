5d5c02a859de50651545f5a1d1e609e1bfcfb97a
Nicolas Venkovic <venkovic@proton.me>
2026-04-18 18:25:36 +0200

    changed stdout buffer in 3d benchmark.

--- bench3d.sh
+++ bench3d.sh
@@ -6,27 +6,27 @@ touch bench3d.out
 
 echo "" >> bench3d.out
 echo "3D isotropic convolution, t = 7" >> bench3d.out
-./bench3d_convolution bench -t 7 >> bench3d.out
+stdbuf -oL ./bench3d_convolution bench -t 7 >> bench3d.out
 
 echo "" >> bench3d.out
 echo "3D isotropic convolution, t = 8" >> bench3d.out
-./bench3d_convolution bench -t 8 >> bench3d.out
+stdbuf -oL ./bench3d_convolution bench -t 8 >> bench3d.out
 
 echo "" >> bench3d.out
 echo "3D isotropic convolution, t = 9" >> bench3d.out
-./bench3d_convolution bench -t 9 >> bench3d.out
+stdbuf -oL ./bench3d_convolution bench -t 9 >> bench3d.out
 
 # 3D anisotropic convolutions
 
 echo "" >> bench3d.out
 echo "3D anisotropic convolution, t = 20" >> bench3d.out
-./bench3d_convolution_anisotropic bench -t 20 >> bench3d.out
+stdbuf -oL ./bench3d_convolution_anisotropic bench -t 20 >> bench3d.out
 
 echo "" >> bench3d.out
 echo "3D anisotropic convolution, t = 22" >> bench3d.out
-./bench3d_convolution_anisotropic bench -t 22 >> bench3d.out
+stdbuf -oL ./bench3d_convolution_anisotropic bench -t 22 >> bench3d.out
 
 echo "" >> bench3d.out
 echo "3D anisotropic convolution, t = 24" >> bench3d.out
-./bench3d_convolution_anisotropic bench -t 24 >> bench3d.out
+stdbuf -oL ./bench3d_convolution_anisotropic bench -t 24 >> bench3d.out
 
